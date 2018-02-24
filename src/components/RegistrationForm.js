034be6b5dd828856556d0743c168b71dc50ceba0
Michael Anderson <michanders@msn.com>
2018-02-24 18:49:55 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,7 +1,11 @@
 class RegistrationForm extends React.Component{
   render(){
     return (
-
+      <form>
+        <input type="text" name="name">
+        <input type="password" name="password">
+        <input type="submit" value="Submit"
+      
     )
   }
 }
